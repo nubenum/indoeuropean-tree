ccf35307bd0eba18a1c28fcd3bed8f7e6f92107d
nubenum <github@nubenum.de>
2018-03-27 18:43:35 +0200

    update lemmata

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 var data = {
     "lemmata" : [
         {
-            "color": "indigo",
+            "color": "darkgreen",
             "langs": [
                 {"l":"pio", "w":"*pH₂tér", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"pge", "w":"*fader-", "en":"", "de":"", "fr":"", "sh":"p -> f"},
-                {"l":"en", "w":"father", "en":"", "de":"", "fr":"", "sh":"p -> f"},
-                {"l":"de", "w":"Vater", "en":"", "de":"", "fr":"", "sh":"p -> f"},
+                {"l":"en", "w":"father", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"de", "w":"Vater", "en":"", "de":"", "fr":"", "sh":"d -> t"},
                 {"l":"fr", "w":"père", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"es", "w":"padre", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"la", "w":"pater", "en":"", "de":"", "fr":"", "sh":""},
@@ -15,6 +15,7 @@ var data = {
                 {"l":"uk", "w":"батько", "en":"batko", "de":"batko", "fr":"batko", "sh":""},
                 {"l":"el", "w":"πατέρας", "en":"patéras", "de":"patéras", "fr":"patéras", "sh":""},
                 {"l":"ga", "w":"athair", "en":"", "de":"", "fr":"", "sh":"p -> -"},
+                {"l":"pce", "w":"*ɸatīr", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"fa", "w":"پدر", "en":"pedr", "de":"pedr", "fr":"pedr", "sh":""}
             ]
         },
@@ -28,8 +29,9 @@ var data = {
                 {"l":"fr", "w":"chien", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"la", "w":"canis", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"el", "w":"κύων", "en":"kúōn", "de":"kúōn", "fr":"kúōn", "sh":""},
-                {"l":"cy", "w":"ci", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"ga", "w":"cù", "en":"", "de":"", "fr":"", "sh":""}
+                {"l":"ga", "w":"cù", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"br", "w":"ki", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pce", "w":"*kū", "en":"", "de":"", "fr":"", "sh":""}
             ]
         },
         {
@@ -51,17 +53,18 @@ var data = {
             "color": "lightsteelblue",
             "langs": [
                 {"l":"pio", "w":"*dwóH₁", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"pge", "w":"*twa- ", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pge", "w":"*twa- ", "en":"", "de":"", "fr":"", "sh":"d -> t"},
                 {"l":"en", "w":"two", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"de", "w":"zwei", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"de", "w":"zwei", "en":"", "de":"", "fr":"", "sh":"t -> ts/z"},
                 {"l":"fr", "w":"deux", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"es", "w":"dos", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"la", "w":"duo", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"el", "w":"δύο", "en":"duo", "de":"duo", "fr":"duo", "sh":""},
                 {"l":"ru", "w":"два", "en":"dva", "de":"dva", "fr":"dva", "sh":""},
                 {"l":"pl", "w":"dwa", "en":"", "de":"", "fr":"", "sh":""},                             
-                {"l":"ga", "w":"dà", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"cy", "w":"dó", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"ga", "w":"dó", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"br", "w":"daou", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pce", "w":"*dwau", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"fa", "w":"دو", "en":"dw", "de":"dw", "fr":"dw", "sh":""}
             ]
         },
@@ -69,29 +72,29 @@ var data = {
             "color": "mediumorchid",
             "langs": [
                 {"l":"pio", "w":"*h₂ébōl", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"pge", "w":"*apluz", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"en", "w":"apple", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"de", "w":"Apfel", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pge", "w":"*apluz", "en":"", "de":"", "fr":"", "sh":"b -> p"},
+                {"l":"en", "w":"apple", "en":"", "de":"", "fr":"", "sh":"b -> p"},
+                {"l":"de", "w":"Apfel", "en":"", "de":"", "fr":"", "sh":"b -> p"},
                 {"l":"ru", "w":"яблоко", "en":"dva", "de":"dva", "fr":"dva", "sh":""},
                 {"l":"pl", "w":"jabłko", "en":"", "de":"", "fr":"", "sh":""},               
                 {"l":"psl", "w":"ѧблоко", "en":"", "de":"", "fr":"", "sh":""},        
                 {"l":"ga", "w":"ubhal", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"cy", "w":"ubhal", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"br", "w":"aval", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"pce", "w":"*abalo-", "en":"", "de":"", "fr":"", "sh":""}
             ]
         },
         {
-            "color": "seagreen",
+            "color": "lightgreen",
             "langs": [
                 {"l":"pio", "w":"*ǵnéwo-", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"pge", "w":"*knewą", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"en", "w":"knee", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"de", "w":"Knie", "en":"", "de":"", "fr":"", "sh":""},  
+                {"l":"pge", "w":"*knewą", "en":"", "de":"", "fr":"", "sh":"g -> k"},
+                {"l":"en", "w":"knee", "en":"", "de":"", "fr":"", "sh":"g -> k -> -"},
+                {"l":"de", "w":"Knie", "en":"", "de":"", "fr":"", "sh":"g -> k"},  
                 {"l":"fr", "w":"genou", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"la", "w":"genu", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"el", "w":"γόνατο", "en":"gónato", "de":"gónato", "fr":"gónato", "sh":""},
-                {"l":"ga", "w":"glùn", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"cy", "w":"glúin", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"ga", "w":"glúin", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"br", "w":"glin", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"pce", "w":"*gnūnos", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"fa", "w":"زانو", "en":"zanu", "de":"zanu", "fr":"zanu", "sh":""}
             ]
@@ -100,8 +103,8 @@ var data = {
             "color": "saddlebrown",
             "langs": [
                 {"l":"pio", "w":"*pélh₁us", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"pge", "w":"*feluz", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"de", "w":"viele", "en":"", "de":"", "fr":"", "sh":""},  
+                {"l":"pge", "w":"*feluz", "en":"", "de":"", "fr":"", "sh":"p -> f"},
+                {"l":"de", "w":"viele", "en":"", "de":"", "fr":"", "sh":"p -> f"},  
                 {"l":"fr", "w":"plus", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"la", "w":"plus", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"el", "w":"πολύς", "en":"polús", "de":"polús", "fr":"polús", "sh":""}
@@ -113,16 +116,48 @@ var data = {
                 {"l":"pio", "w":"*dʰwer-", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"pge", "w":"*durz", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"en", "w":"door", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"de", "w":"Tür", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"la", "w":"forum", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"de", "w":"Tür", "en":"", "de":"", "fr":"", "sh":"d -> t"},
+                {"l":"la", "w":"forum", "en":"meaning enclosed by doors", "de":"Bedeutung eingefasst von Türen", "fr":"signifiant enclos de portes", "sh":""},
                 {"l":"el", "w":"θύρα", "en":"thýra", "de":"thýra", "fr":"thýra", "sh":""},
                 {"l":"ru", "w":"дверь", "en":"dverʹ", "de":"dverʹ", "fr":"dverʹ", "sh":""},
                 {"l":"pl", "w":"drzwi", "en":"", "de":"", "fr":"", "sh":""},               
                 {"l":"psl", "w":"*dvьrь", "en":"", "de":"", "fr":"", "sh":""},        
                 {"l":"ga", "w":"doras", "en":"", "de":"", "fr":"", "sh":""},
-                {"l":"cy", "w":"doras", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"br", "w":"dor", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"pce", "w":"*dworā", "en":"", "de":"", "fr":"", "sh":""},
                 {"l":"fa", "w":"در", "en":"dar", "de":"dar", "fr":"dar", "sh":""}
+            ]
+        },
+        {
+            "color": "navy",
+            "langs": [
+                {"l":"pio", "w":"*dʰugh₂tḗr", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pge", "w":"*duhtēr", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"en", "w":"daughter", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"de", "w":"Tochter", "en":"", "de":"", "fr":"", "sh":"d -> t"},
+                {"l":"el", "w":"θυγάτηρ", "en":"thugátēr", "de":"thugátēr", "fr":"thugátēr", "sh":""},
+                {"l":"ru", "w":"дочь", "en":"dočʹ", "de":"dočʹ", "fr":"dočʹ", "sh":""},
+                {"l":"pl", "w":"córka", "en":"", "de":"", "fr":"", "sh":""},               
+                {"l":"psl", "w":"*dъťi,", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"fa", "w":"دختر", "en":"doxtar", "de":"doxtar", "fr":"doxtar", "sh":""}
+            ]
+        },
+        {
+            "color": "gray",
+            "langs": [
+                {"l":"pio", "w":"*ǵneh₃-", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pge", "w":"*knēaną", "en":"", "de":"", "fr":"", "sh":"g -> k"},
+                {"l":"en", "w":"know", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"de", "w":"kennen", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"fr", "w":"connaître", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"la", "w":"cognoscere", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"el", "w":"γιγνώσκω", "en":"gignṓskō", "de":"gignṓskō", "fr":"gignṓskō", "sh":""},
+                {"l":"ru", "w":"знать", "en":"znatʹ", "de":"znatʹ", "fr":"znatʹ", "sh":""},
+                {"l":"pl", "w":"znać", "en":"", "de":"", "fr":"", "sh":""},               
+                {"l":"psl", "w":"*znati", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"br", "w":"anaout", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"pce", "w":"*gnina-", "en":"", "de":"", "fr":"", "sh":""},
+                {"l":"fa", "w":"شناختن", "en":"šenâxtan", "de":"šenâxtan", "fr":"šenâxtan", "sh":"k -> š"}
             ]
         }
     ],
@@ -134,7 +169,7 @@ var data = {
                     { 
                         "l": "pce",
                         "langs": [
-                            {"l": "ga"}, {"l": "cy"}
+                            {"l": "ga"}, {"l": "br"}
                         ]                    
                     },
                     {
@@ -171,7 +206,8 @@ var data = {
         "la" : {"en":"Latin", "de":"Latein", "fr":"Latin"},
         "el" : {"en":"Greek", "de":"Griechisch", "fr":"Grec"},
         "fa" : {"en":"Persian/Farsi", "de":"Persisch/Farsi", "fr":"Perse/Farsi"},
-        "ga" : {"en":"Gaelic/Irish", "de":"Gälisch/Irisch", "fr":"Gaélique"},
+        "ga" : {"en":"Irish", "de":"Irisch", "fr":"Irlandais"},
+        "br" : {"en":"Breton", "de":"Bretonisch", "fr":"Breton"},
         "cy" : {"en":"Welsh", "de":"Walisisch", "fr":"Gallois"},
         "pce" : {"en":"Proto-Celtic", "de":"Protokeltisch", "fr":"Proto-celtique"},
         "ru" : {"en":"Russian", "de":"Russisch", "fr":"Russe"},
